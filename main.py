deef36f360d57969c32261dbc562a6b00a7f318d
Ole Demay <olesyademy@gmail.com>
2023-12-08 07:15:54 +0100

    Collussions,Score

--- main.py
+++ main.py
@@ -1,12 +1,14 @@
 from turtle import Screen
 from paddle import Paddle
 from ball import Ball
+from scoreboard import Scoreboard
 import time
 
 screen = Screen()
 r_paddle = Paddle((350,0))
 l_paddle = Paddle((-350,0))
 ball = Ball()
+scoreboard = Scoreboard()
 
 screen.bgcolor("black")
 screen.setup(width=800, height=600)
@@ -22,7 +24,7 @@ screen.onkey(l_paddle.go_down, "s")
 
 game_is_on = True
 while game_is_on:
-    time.sleep(0.1)
+    time.sleep(ball.speed_move)
     screen.update()
     ball.move()
 
@@ -30,5 +32,18 @@ while game_is_on:
     if ball.ycor() > 280 or ball.ycor() < -280:
        ball.bounce_y()
 
+    # Detect collusion with paddle
+    if ball.distance(r_paddle) < 50 and ball.xcor() > 320 or ball.distance(l_paddle) < 50 and ball.xcor() > -320:
+        ball.bounce_x()
+
+    # Detect if R paddle misses
+    if ball.xcor() > 380:
+        ball.reset_position()
+        scoreboard.l_point()
+
+  # Detect if L paddle misses
+    if ball.xcor() < -380:
+        ball.reset_position()
+        scoreboard.r_point()
 
 screen.exitonclick()
